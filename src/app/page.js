d3dddc6e0e420bfcc79e20db9dcb6e8ed24c5f7c
JoseRodriguesF <jose.antonio.220507@gmail.com>
2026-08-25 12:29:51 -0300

    style(ui): centralizar informacoes e textos do card de inscricoes do congresso

--- src/app/page.js
+++ src/app/page.js
@@ -209,7 +209,7 @@ export default function Home() {
         <div className="workshop-period-block info-card-period">
           <div className="day-card">
             <div className="info-card__body">
-              <div>
+              <div className="info-card__header">
                 <h3 className="info-card__title">
                   Sobre o Congresso
                 </h3>
